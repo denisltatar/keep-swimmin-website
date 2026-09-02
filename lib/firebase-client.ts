1a4cb226fbd66df4991877a526afc335d970b403
denisltatar <denis.tatar8@gmail.com>
2026-09-01 22:47:42 -0400

    Make Firebase config deployment-safe

--- lib/firebase-client.ts
+++ lib/firebase-client.ts
@@ -7,14 +7,10 @@ const firebaseConfig = {
   projectId: "keepswimmin-ios",
   appId: "1:76251509001:web:6a955ae9d83db80859e3d8",
   storageBucket: "keepswimmin-ios.firebasestorage.app",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "missing-firebase-api-key",
   authDomain: "keepswimmin-ios.firebaseapp.com",
   messagingSenderId: "76251509001",
 };
-
-if (!firebaseConfig.apiKey) {
-  throw new Error("NEXT_PUBLIC_FIREBASE_API_KEY is not configured.");
-}
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
